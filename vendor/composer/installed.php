b30bdc40adf439eb159231917abe1ef492c75f97
tester1337000-max <tester1337000@gmail.com>
2025-09-22 07:17:27 +0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/the-events-calendar',
+        'pretty_version' => 'dev-release/T25.ironman',
+        'version' => 'dev-release/T25.ironman',
+        'reference' => '9ac8b1dc5e1d592b7c59cd7ca2ee26871141dda3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/the-events-calendar' => array(
+            'pretty_version' => 'dev-release/T25.ironman',
+            'version' => 'dev-release/T25.ironman',
+            'reference' => '9ac8b1dc5e1d592b7c59cd7ca2ee26871141dda3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
