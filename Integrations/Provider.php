5b9e539ef90e1d4c1ae24521f427cdc6361b3961
tester1337000-max <tester1337000@gmail.com>
2025-09-22 07:25:46 +0300

    Add files via upload

--- Integrations/Provider.php
+++ Integrations/Provider.php
@@ -1,39 +1,36 @@
 <?php
 /**
- * Handles The Events Calendar integration.
+ * Provides the integrations required by the plugin to work with other plugins.
  *
- * @since   6.0.4
+ * @since   6.0.0
  *
- * @package TEC\Events\Integrations
+ * @package TEC\Events\Custom_Tables\V1\Integrations
  */
 
-namespace TEC\Events\Integrations;
+namespace TEC\Events\Custom_Tables\V1\Integrations;
+
 
 use TEC\Common\Contracts\Service_Provider;
+use TEC\Events\Custom_Tables\V1\Integrations\ACF\Controller as ACF_Controller;
 
 /**
  * Class Provider
  *
- * @since   6.0.4
+ * @since   6.0.0
  *
- * @package TEC\Events\Integrations
+ * @package TEC\Events\Custom_Tables\V1\Integrations
  */
 class Provider extends Service_Provider {
-
 	/**
-	 * Binds and sets up implementations.
+	 * Registers the Service Providers required for the plugin to work with other plugins.
 	 *
-	 * @since 6.0.4
+	 * @since 6.1.4 Removed registering ET CT1 logic.
+	 * @since 6.0.0
 	 */
 	public function register() {
-		$this->container->singleton( static::class, $this );
-
-		$this->container->register( Plugins\WordPress_SEO\Provider::class );
-		$this->container->register( Plugins\Rank_Math\Provider::class );
-		$this->container->register( Plugins\Colbri_Page_Builder\Provider::class );
-		$this->container->register( Plugins\Event_Tickets\Provider::class );
-		$this->container->register( Plugins\Elementor\Controller::class );
-		$this->container->register_on_action( 'tribe_plugins_loaded', Plugins\TEC_Tweaks_Extension\Provider::class );
-		$this->container->register_on_action( 'tec_container_registered_provider_TEC\Tickets_Wallet_Plus\Controller', Plugins\Tickets_Wallet_Plus\Controller::class );
+		// Class defined by the Advanced Custom Fields plugin.
+		if ( class_exists( 'ACF' ) ) {
+			$this->container->register( ACF_Controller::class );
+		}
 	}
 }
